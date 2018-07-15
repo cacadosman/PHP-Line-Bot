4598a128a6634eab5318f96b3494d522dfefb658
cacadosman <wetmanz23@gmail.com>
2018-07-15 21:41:35 +0700

    edit route location

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 
   require __DIR__ . '/../bootstrap/app.php';
   require __DIR__ . '/../bootstrap/container.php';
-  require __DIR__ . '/../routes/api.php';
+  require __DIR__ . '/../api.php';
   
 
   $app->run();
